be028476b21a4dccb1ec9983b54c506b4a9d1c09
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-02-07 09:26:59 +0800

    player movement

--- player.py
+++ player.py
@@ -1,27 +1,29 @@
 import pygame
 from pygame.locals import *
+import math
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
         self.sprites = []
-        self.x = x
-        self.y = y
         self.sprites.append(pygame.image.load('assets/player/player_1.png').convert_alpha())
+        
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
         self.rect = self.image.get_rect(topleft = (x,y))
         
-        self.speed = 500
+        self.speed = 400
 
     def move(self, dt, forwards, sideways):
-        self.y -= forwards * self.speed * dt
-        self.x += sideways * self.speed * dt
+        speed = self.speed * dt
+        if abs(forwards) and abs(sideways):
+            speed *= math.sqrt(2) / 2
+
+        self.rect.y -= forwards * speed
+        self.rect.x += sideways * speed
 
     def update(self, dt, forwards, sideways):
         self.move(dt, forwards, sideways)
-        
-        self.rect = Rect(self.x, self.y, self.rect.width, self.rect.height)
         
         self.image = self.sprites[self.current_sprite]
